5ef7397e1cdd40ba40b3d64a141e7ff5c85c4ffc
onlyman1 <bilolabdumalikov1@gmail.com>
2023-08-09 23:31:07 +0500

    book_detailview to class and generic view

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, ReviewView, ReviewUpdateView
+from .views import BookListView, BookDetailView, ReviewView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='list'),
-    path('<int:id>', BookDetailView.as_view(), name='detail'),
-    path('<int:id>/review/', ReviewView.as_view(), name='review'),
-    path('<int:book_id>/review/<int:review_id>/edit', ReviewUpdateView.as_view(), name='edit_review'),
+    path('<int:pk>', BookDetailView.as_view(), name='detail'),
+    path('<int:pk>/review/', ReviewView.as_view(), name='review'),
 ]
--- books/views.py
+++ books/views.py
@@ -7,30 +7,45 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .forms import ReviewForm
 from .models import Book, AuthorBook, Review
 
 
-class BookDetailView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        author_books = AuthorBook.objects.filter(book=book)
+# class BookDetailView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         author_books = AuthorBook.objects.filter(book=book)
+#
+#         review = ReviewForm()
+#
+#         context = {
+#             'book': book,
+#             'author_books': author_books,
+#             'review': review,
+#         }
+#         return render(request, 'book_detail.html', context)
 
-        review = ReviewForm()
 
-        context = {
-            'book': book,
-            'author_books': author_books,
-            'review': review,
-        }
-        return render(request, 'book_detail.html', context)
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book = self.get_object()
+        context['author_books'] = AuthorBook.objects.filter(book=book)
+        context['review'] = ReviewForm()
+
+        return context
 
 
 class ReviewView(LoginRequiredMixin, View):
-    def post(self, request, id):
-        book = Book.objects.get(id=id)
+    def post(self, request, pk):
+        book = Book.objects.get(pk=pk)
         review = ReviewForm(data=request.POST)
 
         if review.is_valid():
@@ -41,7 +56,7 @@ class ReviewView(LoginRequiredMixin, View):
                 review_text=review.cleaned_data['review_text']
 
             )
-            return redirect(reverse('detail', kwargs={'id': book.id}))
+            return redirect(reverse('detail', kwargs={'pk': book.pk}))
 
         context = {
             'book': book,
@@ -76,39 +91,6 @@ class BookListView(View):
             'page_obj': page_obj,
         }
         return render(request, 'book_list.html', context)
-
-
-
-class ReviewUpdateView(LoginRequiredMixin, View):
-    def get(self, request, book_id, review_id):
-        book = Book.objects.get(id=book_id)
-        review = book.review_set.get(id=review_id)
-        update_review = ReviewForm()
-
-        context = {
-            'update_review': update_review,
-            'book': book,
-            'review': review
-        }
-        return render(request, 'update_review.html', context=context)
-
-    def post(self, request, book_id, review_id):
-        book = Book.objects.get(id=book_id)
-        review = book.review_set.get(id=review_id)
-        update_review = ReviewForm(
-            instance=review,
-            data=request.POST
-        )
-        if update_review.is_valid():
-            update_review.save()
-
-            return redirect('detail', kwargs={'id': book.id})
-        else:
-            context = {
-                'update_review': update_review,
-            }
-            return render(request, 'update_review.html', context)
-
 
 
 
