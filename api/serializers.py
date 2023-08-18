2123bc72acf2f2a40ec204bd41653163aff07c2c
onlyman1 <bilolabdumalikov1@gmail.com>
2023-08-18 21:24:31 +0500

    started

--- api/serializers.py
+++ api/serializers.py
@@ -4,51 +4,25 @@ from books.models import Book, Review
 from users.models import CustomUser
 
 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'isbn', 'description', 'books_picture']
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['pk', 'first_name', 'last_name', 'username', 'email']
+        fields = ['first_name', 'last_name', 'username', 'email']
 
 
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['pk', 'title', 'description', 'isbn']
-
-
-
-
-
-
-
-
-class BookListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['pk', 'title', 'isbn', 'description', 'books_picture']
-
-
-
-
-class ReviewDetailSerializer(serializers.ModelSerializer):
-    user_id = UserSerializer(read_only=True)
-    book_id = BookSerializer(read_only=True)
-
-    user_id_id = serializers.IntegerField(write_only=True)
-    book_id_id = serializers.IntegerField(write_only=True)
+class ReviewSerializer(serializers.ModelSerializer):
+    book_id = BookSerializer()
+    user_id = UserSerializer()
     class Meta:
         model = Review
-        fields = ['pk', 'star_given', 'review_text', 'user_id', 'book_id',
-                  'book_id_id', 'user_id_id']
-
-
-
-
-
-
+        fields = ('star_given', 'review_text', 'user_id', 'book_id')
 
 
 
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from api.views import BookReviewDetailView, BookListView, ReviewsView
+from .views import BookListView, ReviewListView
 
 urlpatterns =[
-    path('reviews/<int:pk>/', BookReviewDetailView.as_view(), name='review_detail'),
-    path('books/', BookListView.as_view(), name='books'),
-    path('reviews/', ReviewsView.as_view(), name='reviews'),
+    path('books/', BookListView.as_view(), name='books_api'),
+    path('reviews/', ReviewListView.as_view(), name='reviews_api'),
 ]
