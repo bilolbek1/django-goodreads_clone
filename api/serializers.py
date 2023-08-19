9486fce306a9c934894a7b245efca557542c5ed0
onlyman1 <bilolabdumalikov1@gmail.com>
2023-08-19 22:45:55 +0500

    Fix errors and testcases

--- api/serializers.py
+++ api/serializers.py
@@ -7,22 +7,31 @@ from users.models import CustomUser
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title', 'isbn', 'description', 'books_picture']
+        fields = ["pk", 'title', 'isbn', 'description', 'books_picture']
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'email']
+        fields = ["pk", 'first_name', 'last_name', 'username', 'email']
 
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    book_id = BookSerializer()
-    user_id = UserSerializer()
+
+    # REVIEW LIST BO'LIB CHIQAYOTGANDA UNING USERI VA BOOKI HAM CHIQISHI UCHUN YARATILDI
+
+    book_id = BookSerializer(read_only=True)
+    user_id = UserSerializer(read_only=True)
+
+    # REVIEW YARATAYOTGANIMIZDA QAYSI KITOBGA VA QAYSI USERDAN EKANLIGINI ANIQLASH UCHUN KERAK BO'LDI,
+    # CHUNKI DJANGODAGI CODIMIZDA HAM USHBU FIELDLAR BO'LADI FAQAT UNDA AVTOMAYIK TO'LDIRILGAN
+
+    book_id_id = serializers.IntegerField(write_only=True)
+    user_id_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = Review
-        fields = ('star_given', 'review_text', 'user_id', 'book_id')
+        fields = ["pk", 'star_given', 'review_text', 'user_id', 'book_id', 'book_id_id', 'user_id_id']
 
 
 
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import BookListView, ReviewListView
+from .views import BookListView, ReviewListView, ReviewDetailView, BookDetailView
 
 urlpatterns =[
     path('books/', BookListView.as_view(), name='books_api'),
     path('reviews/', ReviewListView.as_view(), name='reviews_api'),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review_detail_api'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail_api'),
 ]
