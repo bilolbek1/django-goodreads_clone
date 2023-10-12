d873711f8d7a5bd878705fd694cd296dd4e3124f
onlyman1 <bilolabdumalikov1@gmail.com>
2023-10-12 11:03:24 +0500

    for deploy

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cb0ia9lk&qgj5wpo%(=28uc(*i3=yi$d^zm!+o040x@+w%yj-c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
--- goodreads/wsgi.py
+++ goodreads/wsgi.py
@@ -15,3 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodreads.settings')
 
 application = get_wsgi_application()
 
+app = get_wsgi_application()
