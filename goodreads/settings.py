3edc550881460313d3e82a3490b602fa3766315b
Bilolbek Abdumalikov <bilolabdumalikov1@gmail.com>
2023-12-16 11:05:57 +0500

    added readme

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -78,17 +78,25 @@ WSGI_APPLICATION = 'goodreads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'goodreads_clone',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'USER': 'postgres',
+#         'PASSWORD': '4321'
+#
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'goodreads_clone',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': '4321'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 REST_FRAMEWORK = {
